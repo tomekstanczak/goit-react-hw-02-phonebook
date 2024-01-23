caaa5026f615c6ea29688d6a003e46039c116139
tomekstanczak <tomek.stanczak87@gmail.com>
2024-01-23 12:29:28 +0100

    hw part 2

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -6,30 +6,50 @@ export default class Contacts extends Component {
   constructor() {
     super();
     this.state = {
-      contacts: [],
+      contacts: [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      ],
       name: '',
+      number: '',
+      filter: '',
     };
   }
 
   handleChange = event => {
-    this.setState({ name: event.target.value });
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
   };
 
   addContact = evt => {
     evt.preventDefault();
     const name = this.state.name;
+    const number = this.state.number;
     const prevState = this.state.contacts;
     const newContact = {
       id: nanoid(),
       name,
+      number,
     };
     this.setState({
       contacts: [...prevState, newContact],
     });
-    this.setState({ name: '' });
+    this.setState({
+      name: '',
+      number: '',
+    });
+  };
+  filterContacts = () => {
+    const { contacts, filter } = this.state;
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
   };
 
   render() {
+    const filteredContacts = this.filterContacts();
     return (
       <div>
         <div>
@@ -48,6 +68,18 @@ export default class Contacts extends Component {
               onChange={this.handleChange}
             />
           </label>
+          <label className={css.nameLabel}>
+            Nubmer
+            <input
+              type="tel"
+              name="number"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+              value={this.state.number}
+              onChange={this.handleChange}
+            />
+          </label>
 
           <label>
             <button type="submit">Add contact</button>
@@ -55,9 +87,20 @@ export default class Contacts extends Component {
         </form>
         <div>
           <h2>Contacts</h2>
+          <label className={css.nameLabel}>
+            Find contacts by name
+            <input
+              type="text"
+              name="filter"
+              onChange={this.handleChange}
+              value={this.state.filter}
+            />
+          </label>
           <ul>
-            {this.state.contacts.map(contact => (
-              <li key={contact.id}>{contact.name}</li>
+            {filteredContacts.map(contact => (
+              <li key={contact.id}>
+                {contact.name}: {contact.number}
+              </li>
             ))}
           </ul>
         </div>
